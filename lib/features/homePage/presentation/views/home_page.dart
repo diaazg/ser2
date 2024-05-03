793464c3104e47bbde5cd6dab63d7d0a073e89b9
diaazg <mohamedzellagui585@gmail.com>
2024-05-03 03:35:33 +0100

    ads space done

--- lib/features/homePage/presentation/views/home_page.dart
+++ lib/features/homePage/presentation/views/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ser2/core/utiles/constants.dart';
@@ -63,144 +64,160 @@ class _HomePageState extends State<HomePage> {
               SizedBox(
                 height: size.height * 0.2 * 0.4,
               ),
-              Center(
-                child: SizedBox(
-                  width: 329 * fem,
-                  height: 151 * fem,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        // rectangle127WU5 (1:84)
-                        left: 18 * fem,
-                        top: 116 * fem,
-                        child: Align(
-                          child: SizedBox(
-                            width: 297 * fem,
-                            height: 35 * fem,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20 * fem),
-                                color: const Color(0x7f20b8b8),
+              StreamBuilder(
+             stream: FirebaseFirestore.instance
+          .collection('ads')
+          .doc('advice')
+          .snapshots(),
+                builder: (context, snapshot) {
+                 if(snapshot.hasData){
+                  Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+                   return Center(
+                    child: SizedBox(
+                      width: 329 * fem,
+                      height: 151 * fem,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            // rectangle127WU5 (1:84)
+                            left: 18 * fem,
+                            top: 116 * fem,
+                            child: Align(
+                              child: SizedBox(
+                                width: 297 * fem,
+                                height: 35 * fem,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20 * fem),
+                                    color: const Color(0x7f20b8b8),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                      Positioned(
-                        // rectangle128NmB (1:85)
-                        left: 0 * fem,
-                        top: 0 * fem,
-                        child: Align(
-                          child: SizedBox(
-                            width: 329 * fem,
-                            height: 145 * fem,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20 * fem),
-                                color: const Color(0xff20b8b8),
+                          Positioned(
+                            // rectangle128NmB (1:85)
+                            left: 0 * fem,
+                            top: 0 * fem,
+                            child: Align(
+                              child: SizedBox(
+                                width: 329 * fem,
+                                height: 145 * fem,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20 * fem),
+                                    color: const Color(0xff20b8b8),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                      Positioned(
-                        // doctor5H7T (1:86)
-                        left: 211 * fem,
-                        top: 13 * fem,
-                        child: Align(
-                          child: SizedBox(
-                            width: 110 * fem,
-                            height: 114 * fem,
-                            child: Image.asset(
-                              'assets/page-1/images/doctor-5.png',
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        // vector12yW5 (1:87)
-                        left: 210 * fem,
-                        top: 13 * fem,
-                        child: Align(
-                          child: SizedBox(
-                            width: 111 * fem,
-                            height: 114 * fem,
-                            child: Image.asset(
-                              'assets/page-1/images/vector-12.png',
-                              width: 111 * fem,
-                              height: 114 * fem,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        // besmartbesaferpm (1:88)
-                        left: 25 * fem,
-                        top: 13 * fem,
-                        child: Align(
-                          child: SizedBox(
-                            width: 171 * fem,
-                            height: 30 * fem,
-                            child: DefaultTextStyle(
-                              style: TextStyle(
-                                fontFamily: 'Poppins',
-                                fontSize: 20 * ffem,
-                                fontWeight: FontWeight.w600,
-                                height: 1.5 * ffem / fem,
-                                color: const Color(0xffffffff),
-                              ),
-                              child: const Text(
-                                'Be smart be safe',
-                                textAlign: TextAlign.center,
+                          Positioned(
+                            // doctor5H7T (1:86)
+                            left: 211 * fem,
+                            top: 13 * fem,
+                            child: Align(
+                              child: SizedBox(
+                                width: 110 * fem,
+                                height: 114 * fem,
+                                child: Image.asset(
+                                  'assets/page-1/images/doctor-5.png',
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                      Positioned(
-                        // line9H9P (1:89)
-                        left: 23 * fem,
-                        top: 51 * fem,
-                        child: Align(
-                          child: SizedBox(
-                            width: 152 * fem,
-                            height: 1 * fem,
-                            child: Container(
-                              decoration: const BoxDecoration(
-                                color: Color(0xffffffff),
+                          Positioned(
+                            // vector12yW5 (1:87)
+                            left: 210 * fem,
+                            top: 13 * fem,
+                            child: Align(
+                              child: SizedBox(
+                                width: 111 * fem,
+                                height: 114 * fem,
+                                child: Image.asset(
+                                  'assets/page-1/images/vector-12.png',
+                                  width: 111 * fem,
+                                  height: 114 * fem,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                      Positioned(
-                        // covid19wearmasksaeH (1:90)
-                        left: 29 * fem,
-                        top: 55 * fem,
-                        child: Align(
-                          child: SizedBox(
-                            width: 124 * fem,
-                            height: 60 * fem,
-                            child: DefaultTextStyle(
-                              style: TextStyle(
-                                fontFamily: 'Poppins',
-                                fontSize: 20 * ffem,
-                                fontWeight: FontWeight.w600,
-                                height: 1.5 * ffem / fem,
-                                color: const Color(0xffffffff),
-                              ),
-                              child: const Text(
-                                'Covid-19\nwear masks',
-                                textAlign: TextAlign.center,
+                          Positioned(
+                            // besmartbesaferpm (1:88)
+                            left: 25 * fem,
+                            top: 13 * fem,
+                            child: Align(
+                              child: SizedBox(
+                                width: 171 * fem,
+                                height: 30 * fem,
+                                child: DefaultTextStyle(
+                                  style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 20 * ffem,
+                                    fontWeight: FontWeight.w600,
+                                    height: 1.5 * ffem / fem,
+                                    color: const Color(0xffffffff),
+                                  ),
+                                  child:  Text(
+                                    data['title'].toString(),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
+                          Positioned(
+                            // line9H9P (1:89)
+                            left: 23 * fem,
+                            top: 51 * fem,
+                            child: Align(
+                              child: SizedBox(
+                                width: 152 * fem,
+                                height: 1 * fem,
+                                child: Container(
+                                  decoration: const BoxDecoration(
+                                    color: Color(0xffffffff),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            // covid19wearmasksaeH (1:90)
+                            left: 29 * fem,
+                            top: 55 * fem,
+                            child: Align(
+                              child: SizedBox(
+                                width: 124 * fem,
+                                height: 60 * fem,
+                                child: DefaultTextStyle(
+                                  style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 20 * ffem,
+                                    fontWeight: FontWeight.w600,
+                                    height: 1.5 * ffem / fem,
+                                    color: const Color(0xffffffff),
+                                  ),
+                                  child:  Text(
+                                    data['subTitle'].toString(),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
+                    ),
+                  );
+              
+                 }else if(snapshot.hasError){
+                  return const ErrorCaseWidget(errMessage: 'There is a problem', height: 80, width: 80,);
+                 }else{
+                  return const Center(child: LoadingWidget(size: 100));
+                 }
+                }
               ),
               SizedBox(
                 height: size.height * 0.1 * 0.2,
